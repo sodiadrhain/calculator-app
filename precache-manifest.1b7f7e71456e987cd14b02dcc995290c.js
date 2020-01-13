4c9174b9758f63fbedf008f75de1eedcfb50d12f
sodiadrhain <adesojiawobajo@gmail.com>
2020-01-13 16:28:42 +0100

    Updates

---
+++ precache-manifest.1b7f7e71456e987cd14b02dcc995290c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43465b739999623a19d60d3232c31d7f",
+    "url": "/calculator-app/index.html"
+  },
+  {
+    "revision": "a7650e7ad49aa657a13a",
+    "url": "/calculator-app/static/css/main.69436a26.chunk.css"
+  },
+  {
+    "revision": "19254f7f2f25a7797cb5",
+    "url": "/calculator-app/static/js/2.f0a7ada3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator-app/static/js/2.f0a7ada3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7650e7ad49aa657a13a",
+    "url": "/calculator-app/static/js/main.9919a658.chunk.js"
+  },
+  {
+    "revision": "f6df4e0b3523181c0157",
+    "url": "/calculator-app/static/js/runtime-main.2d74567c.js"
+  }
+]);
